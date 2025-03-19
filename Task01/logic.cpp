a12e847dce4ea282e177a1baa0a9be000135a284
Ivan-Kosty <kivan6386@gmail.com>
2025-03-19 17:31:33 +0300

    Task01 Solution

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -4,9 +4,35 @@
 // Дана математическая прямоугольная матрица размером N на M.
 // Необходимо разработать функцию (или программу), которая 
 // вычисляет среднее арифметическое ненулевых элементов матрицы.
-
+#include <iostream>
 #include "logic.h"
+using namespace std;
 
 double calculate_arithmetical_mean_of_nonzero_elements(int** matrix, int n, int m) {
-	return 0.0;
+	if (n <= 0 || m <= 0) {
+		return 0.0;
+	}
+	if (matrix == nullptr) {
+		return 0.0;
+	}
+	double sum = 0.0;
+	int count = 0;
+
+	for (int i = 0; i < n; ++i) {
+
+		if (matrix[i] == nullptr) {
+			return 0.0;
+		}
+
+		for (int j = 0; j < m; ++j) {
+			if (matrix[i][j] != 0) {
+				sum += matrix[i][j];
+				++count;
+			}
+		}
+	}
+	if (count == 0) {
+		return 0.0;
+	}
+	return sum / count;
 }
