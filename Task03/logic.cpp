23613a6236ba0012465830590b81a8d5b4ecc156
Ivan-Kosty <kivan6386@gmail.com>
2025-03-19 18:13:54 +0300

    Task03 Solution

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -5,9 +5,36 @@
 // Необходимо разработать функцию(или программу), которая находит
 // количество строк, в которых положительных элементов больше 
 // чем всех остальных (отрицательных и нулевых).
-
+#include <iostream>
 #include "logic.h"
-
+using namespace std;
 int count_rows_with_more_positive_values(int** matrix, int n, int m) {
-	return 0;
+    if (n <= 0 || m <= 0) {
+        return 0;
+    }
+    if (matrix == nullptr) {
+        return 0;
+    }
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+
+        if (matrix[i] == nullptr) {
+            continue;
+        }
+        int positive_count = 0;
+        int non_positive_count = 0;
+        for (int j = 0; j < m; ++j) {
+            if (matrix[i][j] > 0) {
+                positive_count++;
+            }
+            else {
+                non_positive_count++;
+            }
+        }
+        if (positive_count > non_positive_count) {
+            count++; 
+        }
+    }
+
+    return count;
 }
